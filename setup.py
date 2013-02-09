6cb91a5816a5d8d8e184487431e3a1172e32002b
Marc Deslauriers <marc.deslauriers@canonical.com>
2013-02-09 11:29:38 -0500

    release 0.22

--- setup.py
+++ setup.py
@@ -86,7 +86,7 @@ class InstallAndUpdateDataDirectory(DistUtilsExtra.auto.install_auto):
 
 DistUtilsExtra.auto.setup(
     name='pasaffe',
-    version='0.21',
+    version='0.22',
     license='GPL-3',
     author='Marc Deslauriers',
     author_email='marc.deslauriers@canonical.com',
