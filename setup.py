102601f6d73bc2fe83adfc3c5bcf4a15df0d23ba
Marc Deslauriers <marc.deslauriers@canonical.com>
2013-09-07 20:23:42 -0400

    properly quote config items

--- setup.py
+++ setup.py
@@ -78,8 +78,8 @@ class InstallAndUpdateDataDirectory(DistUtilsExtra.auto.install_auto):
         # Older DistUtilsExtra put help files in /usr/share/gnome/help and
         # needed a ghelp: URL
         if DistUtilsExtra.auto.__version__ < '2.38':
-            values['__help_prefix__'] = 'ghelp:'
-            values['__help_separator__'] = '#'
+            values['__help_prefix__'] = "'ghelp:'"
+            values['__help_separator__'] = "'#'"
 
         previous_values = update_config(values)
         update_desktop_file(self.prefix + '/share/pasaffe/')
