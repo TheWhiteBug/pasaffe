e6f18fbfd29c8b9394e53b6e05e8a36033487b1f
Marc Deslauriers <marc.deslauriers@canonical.com>
2014-01-22 19:57:22 -0500

    pasaffe_lib/pytwofish.py: fix with python 3

--- pasaffe_lib/pytwofish.py
+++ pasaffe_lib/pytwofish.py
@@ -83,7 +83,7 @@ class Twofish:
         if len(block) % 16:
             raise ValueError("block size must be a multiple of 16")
 
-        plaintext = ''
+        plaintext = b''
 
         while block:
             a, b, c, d = struct.unpack("<4L", block[:16])
@@ -100,7 +100,7 @@ class Twofish:
         if len(block) % 16:
             raise ValueError("block size must be a multiple of 16")
 
-        ciphertext = ''
+        ciphertext = b''
 
         while block:
             a, b, c, d = struct.unpack("<4L", block[0:16])
@@ -333,7 +333,7 @@ def set_key(pkey, in_key, key_len):
     if not pkey.mt_gen:
         gen_mtab(pkey)
         pkey.mt_gen = 1
-    pkey.k_len = (key_len * 8) / 64
+    pkey.k_len = int((key_len * 8) / 64)
 
     a = 0
     b = 0
@@ -472,11 +472,14 @@ def decrypt(pkey, in_blk):
         in_blk[3] = blk[1] ^ pkey.l_key[3]
     return
 
-__testkey = '\xD4\x3B\xB7\x55\x6E\xA3\x2E\x46\xF2\xA2\x82\xB7\xD4' + \
-            '\x5B\x4E\x0D\x57\xFF\x73\x9D\x4D\xC9\x2C\x1B\xD7\xFC' + \
-            '\x01\x70\x0C\xC8\x21\x6F'
-__testdat = '\x90\xAF\xE9\x1B\xB2\x88\x54\x4F\x2C\x32\xDC\x23\x9B\x26\x35\xE6'
-assert 'l\xb4V\x1c@\xbf\n\x97\x05\x93\x1c\xb6\xd4\x08\xe7\xfa' == \
-    Twofish(__testkey).encrypt(__testdat)
-assert __testdat == Twofish(__testkey).decrypt('l\xb4V\x1c@\xbf\n\x97'
-    '\x05\x93\x1c\xb6\xd4\x08\xe7\xfa')
+__testkey = b'\xD4\x3B\xB7\x55\x6E\xA3\x2E\x46\xF2\xA2\x82\xB7\xD4' + \
+            b'\x5B\x4E\x0D\x57\xFF\x73\x9D\x4D\xC9\x2C\x1B\xD7\xFC' + \
+            b'\x01\x70\x0C\xC8\x21\x6F'
+
+__testdat = b'\x90\xAF\xE9\x1B\xB2\x88\x54\x4F\x2C\x32\xDC\x23\x9B\x26\x35\xE6'
+
+__testresult = b'l\xb4V\x1c@\xbf\n\x97\x05\x93\x1c\xb6\xd4\x08\xe7\xfa'
+
+assert Twofish(__testkey).encrypt(__testdat) == __testresult
+
+assert __testdat == Twofish(__testkey).decrypt(__testresult)
