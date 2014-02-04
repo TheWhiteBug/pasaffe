d19987b0b4cebfee99ca6d09e69577d793cb2bff
Marc Deslauriers <marc.deslauriers@canonical.com>
2014-02-03 22:12:26 -0500

    tests/test_lint.py: fix whitespace

--- tests/test_lint.py
+++ tests/test_lint.py
@@ -21,7 +21,7 @@ import subprocess
 class TestPylint(unittest.TestCase):
     def test_project_errors_only(self):
         '''run pylint in error only mode
-        
+
         your code may well work even with pylint errors
         but have some unusual code'''
         return_code = subprocess.call(["pylint", '-E', 'pasaffe'])
@@ -31,7 +31,7 @@ class TestPylint(unittest.TestCase):
     # un-comment the following for loads of diagnostics   
     #~ def test_project_full_report(self):
         #~ '''Only for the brave
-#~ 
+        #~ 
         #~ you will have to make judgement calls about your code standards
         #~ that differ from the norm'''
         #~ return_code = subprocess.call(["pylint", 'pasaffe'])
