486e903c4424bc4cf58fb240991051f837fc7d38
Marc Deslauriers <marc.deslauriers@canonical.com>
2016-02-17 18:57:19 -0500

    specify Gtk version when importing

--- pasaffe/EditDetailsDialog.py
+++ pasaffe/EditDetailsDialog.py
@@ -14,6 +14,8 @@
 # with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
+import gi
+gi.require_version('Gtk', '3.0')
 from gi.repository import Gio, Gtk  # pylint: disable=E0611
 
 from pasaffe_lib.helpersgui import get_builder
